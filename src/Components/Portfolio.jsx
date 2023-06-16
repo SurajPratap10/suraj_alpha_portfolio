05a8f6e3444c9afa5761da4861130eb9c1c15fac
Suraj Pratap <92919173+SurajPratap10@users.noreply.github.com>
2023-06-17 00:01:40 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/pic2.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Man working on a laptop";
 
 /**
  * Project list
